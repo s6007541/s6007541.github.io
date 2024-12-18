f1a11acaedd5ad1795e2bfa4f77e099cf012251a
s6007541 <sorn111930@gmail.com>
2024-12-19 07:22:06 +0900

    update nacbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
   // Get the dropdown button and dropdown content
   const dropdownButton = document.querySelector('.sorn-dropdown-btn');
   const dropdownContent = document.querySelector('.sorn-dropdown-content');
-
+  
   // Function to toggle the dropdown menu
   dropdownButton.addEventListener('click', function(event) {
     // Prevent the event from bubbling up to the document and triggering the outside click
@@ -13,7 +13,5 @@
 
   // Close the dropdown if clicked outside
   document.addEventListener('click', function(event) {
-    if (!dropdownButton.contains(event.target) && !dropdownContent.contains(event.target)) {
       dropdownContent.classList.remove('show');
-    }
   });
