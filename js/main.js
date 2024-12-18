631a2ea7ef9965b3267804d4dd4e747ea18e37c8
s6007541 <sorn111930@gmail.com>
2024-12-19 02:17:46 +0900

    update

--- js/main.js
+++ js/main.js
@@ -135,6 +135,7 @@
 	};
 
 	
+	
 	$(function(){
 		contentWayPoint();
 		goToTop();
---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+  // Get the dropdown button and dropdown content
+  const dropdownButton = document.querySelector('.dropdown-btn');
+  const dropdownContent = document.querySelector('.dropdown-content');
+
+  // Function to toggle the dropdown menu
+  dropdownButton.addEventListener('click', function(event) {
+    // Prevent the event from bubbling up to the document and triggering the outside click
+    event.stopPropagation();
+    
+    // Toggle the 'show' class to trigger the transition
+    dropdownContent.classList.toggle('show');
+  });
+
+  // Close the dropdown if clicked outside
+  document.addEventListener('click', function(event) {
+    if (!dropdownButton.contains(event.target) && !dropdownContent.contains(event.target)) {
+      dropdownContent.classList.remove('show');
+    }
+  });
